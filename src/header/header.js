c5d61466780dac3c9528dc1ac83f8bdd03056868
hadizzo <masimo.hadizzo@gmail.com>
2023-06-15 12:39:33 +0330

    some other changes

--- src/header/header.js
+++ src/header/header.js
@@ -2,8 +2,13 @@ import "./header.css";
 const Header = () => {
   return (
     <div className="mainheader">
+      <p className="animatedtext">Football is the life</p>
+      <p className="subanimatedtext">
+        Football is the life
+        <br /> this is only a text <br /> and this is a new line
+      </p>
+
       <input className="search" type="search" placeholder="Type here" />
-      <button>Search</button>
     </div>
   );
 };
